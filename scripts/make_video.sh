4b5778534da0a2a6f5bbdc2c6958f65a413a0328
TechPro200 <tutankapopo@gmail.com>
2025-11-30 15:36:35 -0400

    version preliminar v2

--- scripts/make_video.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# ffmpeg expects numeric sequence; convert day_000.ppm ... to day_%d.ppm by removing leading zeros in name if needed
-# We'll use pattern day_%03d.ppm directly
-ffmpeg -y -framerate 6 -i output_merge/day_%03d.ppm -c:v libx264 -pix_fmt yuv420p video/brote.mp4
-echo "Video saved to video/brote.mp4"
